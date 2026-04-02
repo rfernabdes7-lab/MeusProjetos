b46d145a1618657dfae57d8b9c0204f6b72de2e6
RenanSoares-Dev <rfernabdes7@gmail.com>
2026-04-02 17:35:50 -0300

    Add files via upload

--- 2-ano-IFTO/java/ativi1.java
+++ 2-ano-IFTO/java/ativi1.java
@@ -1,1 +1,15 @@
+public class ativi1 {
+    public static void main(String[] args) {
+        
 
+
+
+        int gastosjaneiro = 1500;
+
+        int gastosfevereiro = 2300;
+        int gastosdemarço = 17000;
+        int gastostrimestre = gastosjaneiro+gastosfevereiro+gastosdemarço;
+
+        System.out.println("o valor dos gastos é:" + gastostrimestre);
+    }
+}
